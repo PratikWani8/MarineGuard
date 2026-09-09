7ecd4d1859b29472b244db66ea96991e59cfa48d
Pratik Wani <pwani8306@gmail.com>
2026-09-09 23:24:34 +0530

    feat: update dashboard route

--- server/src/routes/dashboardRoutes.js
+++ server/src/routes/dashboardRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { protect } from "../middleware/auth.js";
-import { overview, heatmap } from "../controllers/dashboardController.js";
+import {
+  overview,
+  heatmap,
+  analytics,
+} from "../controllers/dashboardController.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 router.use(protect);
 router.get("/overview", asyncHandler(overview));
 router.get("/heatmap", asyncHandler(heatmap));
+router.get("/analytics", asyncHandler(analytics));
 export default router;
